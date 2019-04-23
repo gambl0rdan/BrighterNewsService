0bf1b7510846e161000b58d2cae35fe430d74c4d
gambl0rdan <39169998+gambl0rdan@users.noreply.github.com>
2019-04-23 22:12:25 +0100

    copying someone elses

--- app.py
+++ app.py
@@ -60,8 +60,12 @@ def get_sentiments():
 # server = WSGIServer(('0.0.0.0', 5001), app)
 # app.debug = True
 # app.run(host = '192.168.1.67',port=5001)
-app.run(host='0.0.0.0', port=5001)
+# app.run(host='0.0.0.0', port=5001)
 
 # server = WSGIServer(('0.0.0.0', 5001), app)
 # server.serve_forever()
 # android:style/TextAppearance.Material.
+
+
+if __name__ == '__main__':
+    app.run()
